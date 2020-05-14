b8b7ca9a76a79834954fdb530d67a543e5cd2442
kota-kota <ko.lespaul0119@gmail.com>
2020-05-14 19:12:35 +0900

    [sample_curl]新規追加

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sample_network
+
+## sample_curl
+
+curlを使ってみるサンプルアプリケーション。
+
+__依存ライブラリ__  
+zlib：1.2.11  
+OpenSSL：1.1.1g  
+curl：7.70.0
---
+++ sample_curl/source/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <stdio.h> 
+#include <curl/curl.h> 
+
+
+int main(void)
+{
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://google.com");
+		res = curl_easy_perform(curl);
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+	system("pause");
+	return 0;
+}
